07326030ea589aab33e7f511ea0eafa376fa8783
zurachu <zurachu@gmail.com>
2021-03-03 09:37:31 +0900

    AudioManager_KanKikuchi

---
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -0,0 +1,15 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// SEファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class SEPath{
+
+	public const string BASI    = "SE/basi";
+	public const string ENTER15 = "SE/enter15";
+	public const string KIN     = "SE/kin";
+	public const string WHISTLE = "SE/whistle";
+
+}
+
+}
