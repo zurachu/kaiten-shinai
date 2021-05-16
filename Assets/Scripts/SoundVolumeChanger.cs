7ab75ca2b7c0a181f88e0d345c90008033101aa9
zurachu <zurachu@gmail.com>
2021-05-16 15:34:50 +0900

    音量調整

---
+++ Assets/Scripts/SoundVolumeChanger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using KanKikuchi.AudioManager;
+
+public class SoundVolumeChanger : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+
+    private static readonly string PrefKey = "SoundVolume";
+
+    private float previousPlayTestVolume;
+
+    private void Start()
+    {
+        var volume = PlayerPrefs.GetFloat(PrefKey, 1f);
+        slider.minValue = 0f;
+        slider.maxValue = 1f;
+        slider.value = volume;
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+        ChangeBaseVolume(volume);
+        previousPlayTestVolume = volume;
+    }
+
+    private void ChangeBaseVolume(float volume)
+    {
+        BGMManager.Instance.ChangeBaseVolume(volume);
+        SEManager.Instance.ChangeBaseVolume(volume);
+    }
+
+    private void OnSliderValueChanged(float value)
+    {
+        PlayerPrefs.SetFloat(PrefKey, value);
+        ChangeBaseVolume(value);
+
+        if (Mathf.Abs(value - previousPlayTestVolume) > 0.1f)
+        {
+            CommonAudioPlayer.PlayButtonClick();
+            previousPlayTestVolume = value;
+        }
+    }
+}
